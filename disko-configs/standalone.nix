2ac6b0275d2ab8808d868504c2cecd44c29d5312
rdean3 <rdean@stickpile.net>
2024-09-17 15:17:05 -0400

    Update more config

---
+++ disko-configs/standalone.nix
@@ -0,0 +1,72 @@
+{ inputs, outputs, lib, pkgs, modulesPath, ... }:{
+  imports = [ 
+    (modulesPath + "/installer/scan/not-detected.nix")
+    inputs.impermanence.nixosModules.impermanence
+    inputs.disko.nixosModule.disko
+    ];
+  disko.devices = {
+    disk = {
+       NixOS = {
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+                priority = 1;
+                name = "ESP";
+                start = "1";
+                end = "1G";
+                type = "EF00";
+                content = {
+                  type = "filesystem";
+                  format = "vfat";
+                  mountpoint = "/boot";
+                };
+            };
+            NixOS = {
+                size = "100%";
+                content = {
+                  type = "btrfs";
+                  extraArgs = [ "-f" ];
+                subvolumes = {
+                  "/stateful" = {
+                      mountOptions = [ "compress=zstd" ];
+                      mountpoint = "/stateful";
+                  };
+                  "/nix-store" = {
+                    mountOptions = [ "compress=zstd" "noatime" ];
+                    mountpoint = "/nix";
+                  };
+                  "/docker" = {
+                      mountOptions = [ "compress=ztd" "noatime" ];
+                      mountpoint = "/var/lib/docker";
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+    nodev = {
+      "/" = {
+          fsType = "tmpfs";
+          mountOptions = [
+              "size = 1G"
+          ];
+      };
+    };
+  };
+# Save some needed stuff
+  fileSystems."/stateful".neededForBoot = true;
+  environment.persistance."/stateful" = {
+    enable = true; 
+    hideMounts = true;
+    directories = [
+        "/var/log"
+        "/var/lib/nixos"
+        "/var/lib/systemd/coredump"
+        "/etc/NetworkManager/system-connections""
+    ];
+  };
+}
