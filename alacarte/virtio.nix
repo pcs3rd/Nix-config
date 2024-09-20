26fdc74ddf0cd30fbe7a518c2e7405d13538fb34
rdean3 <rdean@stickpile.net>
2024-09-20 19:31:46 -0400

    fixed scoping

--- alacarte/virtio.nix
+++ alacarte/virtio.nix
@@ -3,7 +3,4 @@
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
-
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
