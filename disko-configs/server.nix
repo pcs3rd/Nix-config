297f77be8fcf4c6cc7b610f70e103d79a288d596
rdean3 <rdean@stickpile.net>
2024-09-27 11:24:04 -0400

    update more perms

--- disko-configs/server.nix
+++ disko-configs/server.nix
@@ -83,7 +83,7 @@
         "/etc/NetworkManager/system-connections"
         { directory = "/home/manager/.config"; user = "manager"; group = "1000"; mode = "u=rwx,g=rx,o=r"; }
         { directory = "/home/manager/.cache"; user = "manager"; group = "1000"; mode = "u=rwx,g=rx,o=r"; }
-        { directory = "/home/manager/.ssh"; user = "manager"; group = "100"; mode = "u=rwx,g=rx,o=r"; }
+        { directory = "/home/manager/.ssh"; user = "manager"; group = "1000"; mode = "u=rwx,g=rx,o=r"; }
 
     ];
     files = [
