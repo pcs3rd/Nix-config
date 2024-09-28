495f3eead3b1989083c1479362861404bb54950b
rdean3 <rdean@stickpile.net>
2024-09-28 02:26:12 -0400

    bad exe paths

--- alacarte/preferred-server-env.nix
+++ alacarte/preferred-server-env.nix
@@ -18,7 +18,7 @@
     KERNEL!="sd[a-z][0-9]", GOTO="media_by_label_auto_mount_end"
 
     # Import FS infos
-    IMPORT{program}="/sbin/blkid -o udev -p %N"
+    IMPORT{program}="${pkgs.toybox}/blkid -o udev -p %N"
 
     # Get a label if present, otherwise specify one
     ENV{ID_FS_LABEL}!="", ENV{dir_name}="%E{ID_FS_LABEL}"
@@ -30,10 +30,10 @@
     ACTION=="add", ENV{ID_FS_TYPE}=="vfat|ntfs", ENV{mount_options}="$env{mount_options},utf8,gid=100,umask=002"
 
     # Mount the device
-    ACTION=="add", RUN+="/bin/mkdir -p /media/%E{dir_name}", RUN+="/bin/mount -o $env{mount_options} /dev/%k /media/%E{dir_name}"
+    ACTION=="add", RUN+="${pkgs.toybox}/mkdir -p /media/%E{dir_name}", RUN+="${pkgs.toybox}/mount -o $env{mount_options} /dev/%k /media/%E{dir_name}"
 
     # Clean up after removal
-    ACTION=="remove", ENV{dir_name}!="", RUN+="/bin/umount -l /media/%E{dir_name}", RUN+="/bin/rmdir /media/%E{dir_name}"
+    ACTION=="remove", ENV{dir_name}!="", RUN+="${pkgs.toybox}/umount -l /media/%E{dir_name}", RUN+="${pkgs.toybox}/rmdir /media/%E{dir_name}"
 
     # Exit
     LABEL="media_by_label_auto_mount_end"
