f8d07f9b2f2aaf782281298d5fd961b836146dd8
rdean3 <rdean@stickpile.net>
2024-09-28 01:04:31 -0400

    There's some syntax issue somewhere.

--- disko-configs/server.nix
+++ disko-configs/server.nix
@@ -69,8 +69,6 @@
       ];
     };
   };
-
-# Save some needed stuff
   fileSystems."/stateful".neededForBoot = true;
   environment.persistence."/stateful" = {
     enable = true; 
@@ -86,7 +84,6 @@
         { directory = "/home/manager/.ssh"; user = "manager"; group = "1000"; mode = "u=rwx,g=rx,o=r"; }
         { directory = "/home/manager/docker-compose"; user = "manager"; group = "1000"; mode = "u=rwx,g=rx,o=r"; }
         { directory = "/home/manager/scratchpad"; user = "manager"; group = "1000"; mode = "u=rwx,g=rx,o=r"; }
-
     ];
     files = [
       { file = "/etc/machine-id"; parentDirectory = { mode = "u=rwx,g=rwx,o=r"; }; }
@@ -96,7 +93,6 @@
       { file = "/etc/ssh/ssh_host_rsa_key.pub"; parentDirectory = { mode = "u=rwx,g=r,o=r"; }; }
       { file = "/etc/ssh/ssh_host_ed25519_key"; parentDirectory = { mode = "u=rwx,g=r,o=r"; }; }
       { file = "/etc/ssh/ssh_host_ed25519_key.pub"; parentDirectory = { mode = "u=rwx,g=r,o=r"; }; }
-
     ];
   };
 }
