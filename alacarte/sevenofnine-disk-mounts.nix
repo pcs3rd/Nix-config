952bb6df1586a16d64c07d27efd37e4efc0c22b9
rdean3 <rdean@stickpile.net>
2024-10-02 08:28:37 -0400

    Mounting to bad path

--- alacarte/sevenofnine-disk-mounts.nix
+++ alacarte/sevenofnine-disk-mounts.nix
@@ -1,12 +1,12 @@
 { ... }: {
 
 
-  fileSystems."/mnt/disk5" =
+  fileSystems."/Disks/Jellyfin/80b6" =
     { device = "/dev/disk/by-uuid/80b68e30-d9d5-454a-abec-11f7c0c70e58";
       fsType = "ext4";
     };
 
-  fileSystems."/mnt/disk6" =
+  fileSystems."/Disks/Jellyfin/242b" =
     { device = "/dev/disk/by-uuid/242b9249-f2d0-4f81-93d5-54919f03cf3e";
       fsType = "ext4";
     };
