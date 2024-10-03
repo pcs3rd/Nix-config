2be04eb9818b930694d257909f3103eafec80b65
Raymond Dean <18014111+pcs3rd@users.noreply.github.com>
2024-10-02 22:41:12 -0400

    Crap

--- home-configs/raymond.nix
+++ home-configs/raymond.nix
@@ -55,51 +55,10 @@ users.users.rdean = {
       pciutils # lspci
       usbutils # lsusb
       tmux
+      alacritty
       htop 
     ];
     home.stateVersion = "24.05";
 
   };
-  # alacritty - a cross-platform, GPU-accelerated terminal emulator
-  programs.alacritty = {
-    enable = true;
-    # custom settings
-    settings = {
-      env.TERM = "xterm-256color";
-      font = {
-        size = 12;
-        draw_bold_text_with_bright_colors = true;
-      };
-      scrolling.multiplier = 5;
-      selection.save_to_clipboard = true;
-    };
-  };
-
-  programs.bash = {
-    enable = true;
-    enableCompletion = true;
-    # TODO add your custom bashrc here
-    bashrcExtra = ''
-      export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
-    '';
-
-    # set some aliases, feel free to add more or remove some
-    #shellAliases = {
-    #  k = "kubectl";
-    #  urldecode = "python3 -c 'import sys, urllib.parse as ul; print(ul.unquote_plus(sys.stdin.read()))'";
-    #  urlencode = "python3 -c 'import sys, urllib.parse as ul; print(ul.quote_plus(sys.stdin.read()))'";
-    #};
-  };
-
-  # This value determines the home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update home Manager without changing this value. See
-  # the home Manager release notes for a list of state version
-  # changes in each release.
-
-  # Let home Manager install and manage itself.
-  #programs.home-manager.enable = true;
 }
