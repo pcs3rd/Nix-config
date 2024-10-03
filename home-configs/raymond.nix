e3dd59dab6b6af03cca33b65ae385f218cc3a1ca
Raymond Dean <18014111+pcs3rd@users.noreply.github.com>
2024-10-02 22:41:49 -0400

    More

--- home-configs/raymond.nix
+++ home-configs/raymond.nix
@@ -7,27 +7,6 @@ users.users.rdean = {
   description  = "Raymond Dean III";
   extraGroups  = [ "wheel" "networkmanager" ];
 };
-
-  # link the configuration file in current directory to the specified location in home directory
-  # home.file.".config/i3/wallpaper.jpg".source = ./wallpaper.jpg;
-
-  # link all files in `./scripts` to `~/.config/i3/scripts`
-  # home.file.".config/i3/scripts" = {
-  #   source = ./scripts;
-  #   recursive = true;   # link recursively
-  #   executable = true;  # make all files executable
-  # };
-
-  # encode the file content in nix configuration file directly
-  # home.file.".xxx".text = ''
-  #     xxx
-  # '';
-
-  # set cursor size and dpi for 4k monitor
-  xresources.properties = {
-    "Xcursor.size" = 16;
-    "Xft.dpi" = 172;
-  };
   home-manager.users.rdean = {
     home.homeDirectory = "/home/rdean";
     home.packages = with pkgs; [
