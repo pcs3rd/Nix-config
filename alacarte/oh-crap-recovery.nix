89e9499eb60b324372e9121a1894d75c75fb3d04
Raymond Dean <18014111+pcs3rd@users.noreply.github.com>
2024-10-02 22:01:47 -0400

    typo

--- alacarte/oh-crap-recovery.nix
+++ alacarte/oh-crap-recovery.nix
@@ -15,7 +15,7 @@
         pkgs.gparted
       ];
       services.xserver.displayManager.gdm.enable = lib.mkForce false;
-      service.xserver.desktopManager.gnome.enable = lib.mkForce false;
+      services.xserver.desktopManager.gnome.enable = lib.mkForce false;
       services.xserver.windowManager.openbox.enable = true;
       services.xserver.displayManager.lightdm.enable = true;
       services.displayManager.autoLogin.enable = true;
