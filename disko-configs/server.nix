c60acb7bbd74e25cffd25a391f679882f62582f7
Raymond Dean <18014111+pcs3rd@users.noreply.github.com>
2024-10-15 15:03:05 -0400

    try to use .gitconfig

--- disko-configs/server.nix
+++ disko-configs/server.nix
@@ -90,7 +90,7 @@
       #{ file = "/home/manager/.bash_history"; parentDirectory = { mode = "u=rwx,g=rwx,o=r"; }; }
       "/home/manager/.gitconfig"
       "/home/manager/.bash_history"
-      #{ file = "/home/manager/.gitconfig"; parentDirectory = { mode = "u=rwx,g=rwx,o=r"; }; }
+      { file = "/home/manager/.gitconfig"; parentDirectory = { mode = "u=rwx,g=rwx,o=r"; }; }
       { file = "/etc/ssh/ssh_host_rsa_key"; parentDirectory = { mode = "u=rwx,g=r,o=r"; }; }
       { file = "/etc/ssh/ssh_host_rsa_key.pub"; parentDirectory = { mode = "u=rwx,g=r,o=r"; }; }
       { file = "/etc/ssh/ssh_host_ed25519_key"; parentDirectory = { mode = "u=rwx,g=r,o=r"; }; }
