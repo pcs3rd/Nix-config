7288d89d0e86e4b36557a98e2379be422154260f
Raymond Dean <18014111+pcs3rd@users.noreply.github.com>
2024-11-14 14:36:58 -0500

    Update gnome-desktop.nix

--- alacarte/gnome-desktop.nix
+++ alacarte/gnome-desktop.nix
@@ -2,5 +2,9 @@
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
-
+  environment.gnome.excludePackages = with pkgs; [
+    epiphany # web browser
+    gnome.geary # email reader. Up to 24.05. Starting from 24.11 the package name is just geary.
+    evince # document viewer
+  ];
 }
