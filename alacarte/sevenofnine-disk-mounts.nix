388cd4a26acd38a57bb6e7cce042474ed3adabeb
Raymond Dean <18014111+pcs3rd@users.noreply.github.com>
2024-11-25 13:19:31 -0500

    Update sevenofnine-disk-mounts.nix

--- alacarte/sevenofnine-disk-mounts.nix
+++ alacarte/sevenofnine-disk-mounts.nix
@@ -1,39 +1,34 @@
 { pkgs, ... }: {
-  fileSystems."/Disks/Jellyfin/80b6" =
+  fileSystems."/Disks/Jellyfin/80b6" = # Media
     { device = "/dev/disk/by-uuid/80b68e30-d9d5-454a-abec-11f7c0c70e58";
       fsType = "ext4";
     };
 
-  fileSystems."/Disks/Jellyfin/242b" =
+  fileSystems."/Disks/Jellyfin/242b" = # Media
     { device = "/dev/disk/by-uuid/242b9249-f2d0-4f81-93d5-54919f03cf3e";
       fsType = "ext4";
     };
 
-  fileSystems."/persist" =
+  fileSystems."/persist" = #appdata and crap
     { device = "/dev/disk/by-uuid/fdb0778a-4425-4fc1-9bc9-e9e1fd1550ec";
       fsType = "btrfs";
       options = [ "subvol=AppData" "compress=zstd" ];
-    };
-  fileSystems."/persist/prod/web/ownfoil/games" =
-    { device = "/dev/disk/by-uuid/ac48b9f0-f25c-4d04-bd43-b50d45cdd101";
-      fsType = "btrfs";
-      options = [ "subvol=games" "compress=zstd" ];
     };
 
   #Make sure rclone is there  
   environment.systemPackages = [ pkgs.rclone ];
 
-  fileSystems."/persist/remote-downloads" = {
-    device = "seedbox:/downloads/manual";
-    fsType = "rclone";
-    options = [
-      "nodev"
-      "nofail"
-      "allow_other"
-      "args2env"
-      "config=/stateful/sys-data/rclone-mnt.conf"
-      "uid=911" 
-      "gid=911" 
-    ];
-  };
+#  fileSystems."/persist/remote-downloads" = { #This is not in use anymore, but am keeping for future reference.
+#    device = "seedbox:/downloads/manual";
+#    fsType = "rclone";
+#    options = [
+#      "nodev"
+#      "nofail"
+#      "allow_other"
+#      "args2env"
+#      "config=/stateful/sys-data/rclone-mnt.conf"
+#      "uid=911" 
+#      "gid=911" 
+#    ];
+#  };
 }
