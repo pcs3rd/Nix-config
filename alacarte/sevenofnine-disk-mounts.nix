fd2cb564acf824f2e67cafa656d953cb108f98b4
Raymond Dean <18014111+pcs3rd@users.noreply.github.com>
2024-11-25 15:05:44 -0500

    Update sevenofnine-disk-mounts.nix

--- alacarte/sevenofnine-disk-mounts.nix
+++ alacarte/sevenofnine-disk-mounts.nix
@@ -8,12 +8,17 @@
     { device = "/dev/disk/by-uuid/242b9249-f2d0-4f81-93d5-54919f03cf3e";
       fsType = "ext4";
     };
-
+   fileSystems."/Disks/Jellyfin/2111" =
+      { device = "/dev/disk/by-uuid/2111d93a-159d-4a2c-8fc5-24b93026e254";
+        fsType = "btrfs";
+        options = [ "subvol=media" ];
+      };
   fileSystems."/persist" = #appdata and crap
     { device = "/dev/disk/by-uuid/fdb0778a-4425-4fc1-9bc9-e9e1fd1550ec";
       fsType = "btrfs";
       options = [ "subvol=AppData" "compress=zstd" ];
     };
+
 
   #Make sure rclone is there  
   environment.systemPackages = [ pkgs.rclone ];
