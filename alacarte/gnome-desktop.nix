c63fb967b3935e781f0c9dc45b45e5de65c4d54f
Raymond Dean <18014111+pcs3rd@users.noreply.github.com>
2024-12-04 18:26:13 -0500

    Update gnome-desktop.nix

--- alacarte/gnome-desktop.nix
+++ alacarte/gnome-desktop.nix
@@ -2,7 +2,4 @@
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
-  environment.gnome.excludePackages = with pkgs; [
-    geary # email reader. Up to 24.05. Starting from 24.11 the package name is just geary.
-  ];
 }
