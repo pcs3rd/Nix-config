035ed607dc0f83db43fef897fdab8fcf706b3ae6
Raymond Dean <18014111+pcs3rd@users.noreply.github.com>
2025-04-03 06:17:11 -0400

    config

--- alacarte/phosh.nix
+++ alacarte/phosh.nix
@@ -7,34 +7,5 @@
 { config, lib, pkgs, options, ... }:
 
 {
-  mobile.beautification = {
-    silentBoot = lib.mkDefault true;
-    splash = lib.mkDefault true;
-  };
 
-  services.xserver.desktopManager.phosh = {
-    enable = true;
-    group = "users";
-  };
-
-  programs.calls.enable = true;
-
-  environment.systemPackages = with pkgs; [
-    # Disabled since it uses `olm` which was marked insecure.
-    #chatty              # IM and SMS
-    epiphany            # Web browser
-    gnome-console       # Terminal
-    megapixels          # Camera
-  ];
-
-  hardware.sensor.iio.enable = true;
-
-  assertions = [
-    { assertion = options.services.xserver.desktopManager.phosh.user.isDefined;
-    message = ''
-      `services.xserver.desktopManager.phosh.user` not set.
-        When importing the phosh configuration in your system, you need to set `services.xserver.desktopManager.phosh.user` to the username of the session user.
-    '';
-    }
-  ];
 }
