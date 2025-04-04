8d16ffde2bedf0dac851dde34e473098c5dbdc75
Raymond Dean <18014111+pcs3rd@users.noreply.github.com>
2025-04-04 14:04:50 -0400

    Update kodama.nix

--- base-configs/kodama.nix
+++ base-configs/kodama.nix
@@ -2,7 +2,7 @@
 
   {
     config = {
-      nixpkgs.hostPlatform = lib.mkDefault "aarch64-linux";
+      pkgs.stdenv.hostPlatform = lib.mkDefault "aarch64-linux";
 
       mobile.beautification = {
         silentBoot = lib.mkDefault true;
