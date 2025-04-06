898e4a6cf2112e3e4686361c85bea969657e5121
Raymond Dean <18014111+pcs3rd@users.noreply.github.com>
2025-04-06 06:33:13 -0400

    initial router stuff

--- base-configs/bl460c.nix
+++ base-configs/bl460c.nix
@@ -7,12 +7,57 @@
 
 # Network
   networking.networkmanager.enable = true;
-  networking = {
-    vlans = {
-      vlan2 = {id=2; interface="enp2s0f1"; }; # these hosts show up all willy-nilly on my unifi console. This might fix it
-    };
+  boot.kernel.sysctl = {
+    # Enable IPv4 forwarding
+    "net.ipv4.conf.all.forwarding" = true;
+
+    # Disable IPv6 Forwarding
+    "net.ipv6.conf.all.forwarding" = false;
+
+    # source: https://github.com/mdlayher/homelab/blob/master/nixos/routnerr-2/configuration.nix#L52
+    # By default, not automatically configure any IPv6 addresses.
+    "net.ipv6.conf.all.accept_ra" = 0;
+    "net.ipv6.conf.all.autoconf" = 0;
+    "net.ipv6.conf.all.use_tempaddr" = 0;
+
+    # On WAN, allow IPv6 autoconfiguration and tempory address use.
+    "net.ipv6.conf.${name}.accept_ra" = 2;
+    "net.ipv6.conf.${name}.autoconf" = 1;
   };
 
+  # Enable Bind for local DNS Resolving
+  services.bind.enable = true;
+  networking = {
+    useDHCP = false;
+    hostName = "router";
+    nameserver = [ "<DNS IP>" ];
+    # Define VLANS
+    vlans = {
+      upstream = {
+        id = 10;
+        interface = "enp1s0";
+      };
+      clusterBackplane = {
+        id = 20;
+        interface = "enp2s0";
+      };
+    };
+
+    interfaces = {
+      # Don't request DHCP on the physical interfaces
+      enp1s0.useDHCP = false;
+      enp2s0.useDHCP = false;
+      
+      # Handle the VLANs
+      wan.useDHCP = false;
+      lan = {
+        ipv4.addresses = [{
+          address = "10.1.1.1";
+          prefixLength = 28;
+        }];
+      };
+    };
+  }; 
 
   time.timeZone = "America/New_York";
   networking.timeServers = options.networking.timeServers.default ++ [ "time.google.com" ]; 
@@ -20,16 +65,16 @@
 
 # User stuff
   users.users = {
-    manager = {
+    router = {
       isNormalUser = true;
-      home = "/home/manager";
+      home = "/home/router";
       description  = "manager user for ssh access";
-      uid = 1000; 
+      uid = 1111; 
       extraGroups = [ "wheel" "docker" "networkmanager" "storage" ]; 
-      hashedPasswordFile = "/stateful/sys-data/manager-passwordHash"; #  mkpasswd -m sha-512 
+      hashedPasswordFile = "/stateful/sys-data/router-passwordHash"; #  mkpasswd -m sha-512 
     };
   };
-  system.stateVersion = "24.05";
+  system.stateVersion = "24.11";
   boot.initrd.availableKernelModules = [ "uhci_hcd" "ehci_pci" "hpsa" "usbhid" "usb_storage" "sd_mod" "sr_mod" ];  
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
@@ -37,50 +82,5 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-
-# Rclone config for appdata storage
-
-  #Make sure rclone is there  
-  environment.systemPackages = [ pkgs.rclone ];
-  
-  fileSystems."/persist/dev" = {
-    device = "AppData:/persist/dev"; # force correct path
-    fsType = "rclone";
-    options = [
-      "nodev"
-      "nofail"
-      "allow_other"
-      "args2env"
-      "config=/stateful/sys-data/rclone-mnt.conf"
-      "uid=911" 
-      "gid=911" 
-    ];
-  };
-  fileSystems."/persist/downloads" = {
-    device = "AppData:/persist/downloads";
-    fsType = "rclone";
-    options = [
-      "nodev"
-      "nofail"
-      "allow_other"
-      "args2env"
-      "config=/stateful/sys-data/rclone-mnt.conf"
-      "uid=911" 
-      "gid=911" 
-    ];
-  };
-  fileSystems."/persist/remote-downloads" = {
-    device = "seedbox:/downloads/manual";
-    fsType = "rclone";
-    options = [
-      "nodev"
-      "nofail"
-      "allow_other"
-      "args2env"
-      "config=/stateful/sys-data/rclone-mnt.conf"
-      "uid=911" 
-      "gid=911" 
-    ];
-  };
 
 }
