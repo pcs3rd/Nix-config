7a845f766ec1e0b4677081f9140d601f1f8c7848
Raymond Dean <18014111+pcs3rd@users.noreply.github.com>
2025-05-04 11:17:30 -0400

    verbose mkdir

--- overlay-packages/pam_oauth2_device/default.nix
+++ overlay-packages/pam_oauth2_device/default.nix
@@ -35,8 +35,8 @@ pkgs.stdenv.mkDerivation rec {
   '';
 
   installPhase = ''
-    mkdir -p $out/bin $out/lib/security
-    mkdir -p $out/etc/pam_oauth2_device/
+    mkdir -p -v $out/bin $out/lib/security
+    mkdir -p -v $out/etc/pam_oauth2_device/
     cp pam_oauth2_device.so $out/lib/security
     cp config_template.json /etc/pam_oauth2_device/config.json
   '';
