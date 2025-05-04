085d06af3e03e8a82b796ae4811ec30d49003563
Raymond Dean <18014111+pcs3rd@users.noreply.github.com>
2025-05-04 10:19:33 -0400

    update stuff

--- overlay-packages/pam_oauth2_device/default.nix
+++ overlay-packages/pam_oauth2_device/default.nix
@@ -1,9 +1,12 @@
 {
-  lib,
+  pkgs ? import <nixpkgs> { system = builtins.currentSystem; }
+  lib ? pkgs.lib,
+  stdenv ? pkgs.stdenv,
   pam,
   fetchFromGitHub,
   autoreconfHook
 }:
+
 
 let 
   pkgs =  import <nixpkgs> {}; # bring all of Nixpkgs into scope
