ea30e387186b29157f093878e4baa3ffc8f5502c
Raymond Dean <18014111+pcs3rd@users.noreply.github.com>
2025-06-16 21:40:01 -0400

    update gnome stuff

--- alacarte/gnome-desktop.nix
+++ alacarte/gnome-desktop.nix
@@ -1,4 +1,13 @@
-{
+{ inputs, outputs, lib, pkgs, ... }:{
+    environment.defaultPackages = lib.mkForce [
+        pkgs.git
+        pkgs.gh
+        pkgs.gnomeExtensions.tailscale-status
+        pkgs.gnomeExtensions.dock-from-dash
+        pkgs.gnomeExtensions.frippery-applications-menu
+        pkgs.gnomeExtensions.clipboard-indicator
+        
+    ];
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
