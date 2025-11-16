16d140b7ac6534f1fbce6f60b892d3bd5882acc7
Raymond Dean <18014111+pcs3rd@users.noreply.github.com>
2025-11-15 21:01:46 -0500

    af

--- flake.nix
+++ flake.nix
@@ -162,7 +162,7 @@
             ./alacarte/preferred-server-env.nix
             ./alacarte/tailscale.nix
             ./alacarte/docker.nix
-            ./alacarte/grub.nix
+            ./alacarte/arm-grub.nix
             {
               networking.hostName = "dev";
               boot.loader.grub.device = "/dev/vda";
