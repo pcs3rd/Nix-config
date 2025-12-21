5ae23d15e6befc7a9555c7da3c41f5c46f911c67
pcs3rd <rhean1620@gmail.com>
2025-12-21 18:00:12 -0500

    f

--- alacarte/steam-radeon.nix
+++ alacarte/steam-radeon.nix
@@ -42,12 +42,12 @@
         capSysAdmin = true;
         openFirewall = true;
     };
-    security.wrappers.sunshine = {
-            owner = "root";
-            group = "root";
-            capabilities = "cap_sys_admin+p";
-            source = "${pkgs.sunshine}/bin/sunshine";
-    };
+#    security.wrappers.sunshine = {
+#            owner = "root";
+#            group = "root";
+#            capabilities = "cap_sys_admin+p";
+#            source = "${pkgs.sunshine}/bin/sunshine";
+#    };
     services.avahi.publish.enable = true;
     services.avahi.publish.userServices = true;
     programs.steam = {
@@ -102,6 +102,6 @@
     services.displayManager.autoLogin = {
         enable = true;
         # Replace `yourUserName` with the actual username of user who should be automatically logged in
-        user = "rdean3";
+        #user = "rdean3";
     };
 }
