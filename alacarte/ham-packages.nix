276abd539a81c33b5a25d7a91b34db4526949c9c
Raymond Dean <18014111+pcs3rd@users.noreply.github.com>
2025-12-21 01:42:10 -0500

    Update ham-packages.nix
    Not a tone of space on this dev

--- alacarte/ham-packages.nix
+++ alacarte/ham-packages.nix
@@ -1,35 +1,19 @@
 { inputs, outputs, lib, pkgs, ... }:{
   environment.systemPackages = with pkgs; [
-    contact
     python313Packages.meshtastic
     wsjtx
     fldigi
-    hamrs
     hamlib
-    hamtransfer
-    ax25-apps
-    digiham
-    tlf
-    grig
-    klog
-    libax25
-    flex-ncat
     gpredict
     soundmodem
-    grig
     direwolf
     cqrlog
     flrig
     js8call
-    openwebrx
-    sigdigger
-    sigutils
     suwidgets
     qradiolink
     digiham
     carla
 		firefox
-    calf
-
   ];
 }
