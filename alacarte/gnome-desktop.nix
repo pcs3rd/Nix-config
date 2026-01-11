d17cc025fa2d052066af14667006f28bd9d7f188
Raymond Dean <18014111+pcs3rd@users.noreply.github.com>
2026-01-11 11:01:58 -0500

    Enable networkmanager for gnome

--- alacarte/gnome-desktop.nix
+++ alacarte/gnome-desktop.nix
@@ -11,4 +11,5 @@
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+  networking.networkmanager.enable = true;
 }
