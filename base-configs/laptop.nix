10b2867de623a5f9227dca3b86d135ef9caa0769
Raymond Dean <18014111+pcs3rd@users.noreply.github.com>
2026-01-15 13:53:14 -0500

    Move back to systemd-boot

--- base-configs/laptop.nix
+++ base-configs/laptop.nix
@@ -8,7 +8,7 @@
   nixpkgs.config.allowUnfree = true;
 
   boot.initrd.systemd.enable = true;
-  boot.loader.limine.enable = true;
+  boot.loader.systemd-boot.enable = true;
   boot.loader.systemd-boot.configurationLimit = 2;
   boot.loader.efi.canTouchEfiVariables = true;
   boot = {
