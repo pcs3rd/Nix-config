6d433eb3bc723ae37ce0eda879370db36afdddf3
Raymond <rdean@stickpile.net>
2026-05-05 01:30:50 -0400

    fixed

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
     #nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 		nixos-hardware.url = "github:8bitbuddhist/nixos-hardware?ref=surface-rust-target-spec-fix";
     # Bitfocus Companion modules
-    inputs.companion.url = "github:pcs3rd/bitfocus-companion-flake";
+    companion.url = "github:noblepayne/bitfocus-companion-flake";
   };
 
   outputs = {
