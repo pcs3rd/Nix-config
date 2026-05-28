4ce1a40a4840d38b33615c7192b49dd836555903
pcs3rd <rhean1620@gmail.com>
2026-05-28 11:24:57 -0400

    configure network adapter

--- base-configs/server.nix
+++ base-configs/server.nix
@@ -13,6 +13,20 @@
   
   # Enable SPICE guest agent
   services.spice-vdagentd.enable = true; 
+# Network adapt for nfs
+  networking = {
+    interfaces.enp5s0 = {
+      ipv4.addresses = [{
+        address = "192.168.100.10";
+        prefixLength = 24;
+      }];
+    };
+    defaultGateway = {
+      address = "192.168.100.1";
+      interface = "ens3";
+    };
+
+  };
 # User stuff
   users.users = {
 
