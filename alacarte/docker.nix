9cfccf570457e2bdb273dd138732daede45f0100
pcs3rd <rhean1620@gmail.com>
2026-05-31 07:50:05 -0400

    docker containers not self-recovering after boot

--- alacarte/docker.nix
+++ alacarte/docker.nix
@@ -2,4 +2,13 @@
   virtualisation.docker.enable = true;
   virtualisation.oci-containers.backend = "docker";
   virtualisation.docker.liveRestore = false; # This breaks swarms
+  # Have docker start after network
+  systemd.services.my-service = {
+    after = [ "network-online.target" ];
+    wants = [ "network-online.target" ];
+
+    before = [ "multi-user.target" ];
+
+    wantedBy = [ "multi-user.target" ];
+  };
 }
