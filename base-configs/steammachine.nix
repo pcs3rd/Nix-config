e364ac5cd8b455057739354f90b57996939dd78f
pcs3rd <rhean1620@gmail.com>
2026-07-02 10:34:10 -0400

    audio

--- base-configs/steammachine.nix
+++ base-configs/steammachine.nix
@@ -54,12 +54,12 @@
   # Audio   #
   ###########
   services.pipewire = {
-    enable = true; # if not already enabled
+    enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment the following
     jack.enable = true;
+    pulse.enable = true;
+    wireplumber.enable = true;
   };
 
   ###########
